61843cbf8493bfffd7d1c0d618f13db941cd83c4
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-06-18 21:05:33 +0530

    feat: Added Job List Page with Sidebar and Filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
+import JobFilterSidebar from "@/components/JobFilterSidebar";
+import JobListItem from "@/components/JobListItem";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const jobs = await prisma.job.findMany({
+    where: { approved: true },
+    orderBy: { createdAt: "desc" },
+  });
   return (
     <>
-      <div className="text-red-500">hii</div>
+      <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
+        <div className="space-y-5 text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Developer Jobs
+          </h1>
+          <p className="text-muted-foreground">Find your dream job.</p>
+        </div>
+        <section className="flex flex-col gap-4 md:flex-row">
+          <JobFilterSidebar />
+          <div className="space-y-4 ">
+            {jobs.map((job) => (
+              <JobListItem job={job} key={job.id} />
+            ))}
+          </div>
+        </section>
+      </main>
     </>
   );
 }
